9ab722e7798693e3c85ecd1a0fec0df053b632f6
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2026-04-04 02:53:02 +0800

    feat: added logic to the main sender file (not tested yet)

--- Main Specs/Sender_Main.py
+++ Main Specs/Sender_Main.py
@@ -1,26 +1,73 @@
-#Send INVITE (PAT)
+import socket
+from Packets import SIP_packet
+from Connection_Functions.Send import Send_func
 
-    #place code here
+SENDER_IP = ""
+SENDER_PORT = 0
+flag = True
+    
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
+sock.bind((SENDER_IP, SENDER_PORT))
+#set timeout to 5 seconds
+sock.settimeout(5.0)
+
+print(f"UDP server listening on {SENDER_IP}:{SENDER_PORT}\n\n")
+
+#Add the address and port of the receiver over here!
+sender = Send_func(sender_ip=SENDER_IP, sender_port=SENDER_PORT, receiver_ip="", receiver_port=0, socket=sock)
+
+while flag:
+    print("Press Enter to send an INVITE...")
+    input()
+
+    sender.send_invite()
+
+    print(f"\n\nSending INVITE to {sender.receiver_ip}:{sender.receiver_port}...\n\n")
+
+    try:
+        data, addr = sock.recvfrom(4096)
+        packet = SIP_packet.from_bytes(data)
+
+        if packet.start_line.startswith("SIP/2.0 200 OK"):
+            print(f"Received 200 OK from {addr}.\n\n")
+            sender.send_ack(packet)
+            flag = False
+        else:
+            print(f"Received unexpected response from {addr}: {packet.start_line}")
+            sender.send_ack(packet)
+
+    except socket.timeout:
+        print(f"Timeout: No response from {sender.receiver_ip}:{sender.receiver_port}. Please try again.")
+
+flag = True
+
+while flag:
+    print("Type 'S' to send an audio file or 'B' to end the call...\n\n")
+    choice = input().upper()
+
+    if choice == 'S':
+        #Ask user for file -> Loop through file and blast RTP packets (JERU)
+        pass
+    elif choice == 'B':
+        sender.send_bye()
+
+        print(f"\n\nEnding connection with {sender.receiver_ip}:{sender.receiver_port}...\n\n")
+
+        try:
+            data, addr = sock.recvfrom(4096)
+            packet = SIP_packet.from_bytes(data)
+
+            if packet.start_line.startswith("SIP/2.0 200 OK"):
+                print(f"Received 200 OK from {addr}.\n\n")
+                flag = False
+            else:
+                print(f"Received unexpected response from {addr}: {packet.start_line}. Still ending the program.")
+                flag = False
+
+        except socket.timeout:
+            print(f"Timeout: No response from {sender.receiver_ip}:{sender.receiver_port}. Ending the Program.\n\n")
+            flag = False
 
 
-#Wait for 200 OK (PAT)
-
-    #place code here
-
-#Send ACK (PAT)
-
-    #place code here
-
-#Ask user for file -> Loop through file and blast RTP packets (JERU)
-
-    #place code here
-
-#File ends (JERU) -> Send BYE (PAT) 
-
-    #place code here
-
-#Wait for 200 OK -> Close application (PAT)
-
-    #place code here
-
-#Delete comments after implementation, i ai'd the comments i made so mas solid definitions nito kesa sa orig HAHHAHHAHAH.
+print("Thank you for using the program...")
+sock.close()
