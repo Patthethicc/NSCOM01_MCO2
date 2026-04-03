42a34e57e94b39099993497d55828691a3075347
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2026-04-04 02:19:36 +0800

    refactor: turned Send and Receive files into classes

--- Connection_Functions/Receive.py
+++ Connection_Functions/Receive.py
@@ -1,82 +1,90 @@
 from Packets.SDP_packet import SDP_packet
 from Packets.SIP_packet import SIP_packet
 
-def recv_invite(receiver_ip, receiver_port, packet, socket, sender_ip, sender_port):
-    try:
-        sdp = SDP_packet(origin=f"- 696969 696969 IN IP {receiver_ip}", connection_data=f"IN IP {receiver_ip}", 
-            media=f"audio {receiver_port} RTP/AVP 0", attribute="rtpmap:0 PCMU/8000").to_string()
+class Recv_func:
+    def __init__(self, receiver_ip, receiver_port, socket, sender_ip, sender_port):
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port
+        self.socket = socket
+        self.sender_ip = sender_ip
+        self.sender_port = sender_port
+
+    def recv_invite(self, packet):
+        try:
+            sdp = SDP_packet(origin=f"- 696969 696969 IN IP {self.receiver_ip}", connection_data=f"IN IP {self.receiver_ip}", 
+                media=f"audio {self.receiver_port} RTP/AVP 0", attribute="rtpmap:0 PCMU/8000").to_string()
+            
+            start_line = SIP_packet.make_start_line_receive("200", "OK")
+
+            sip = SIP_packet(start_line=start_line, 
+                via=packet.via,         
+                to=packet.to,           
+                _from=packet._from,     
+                call_id=packet.call_id,
+                cseq=packet.cseq,
+                content_type="application/sdp",
+                content_length=len(sdp),
+                body=sdp)
+            
+            sip_string = sip.to_bytes()
+
+            self.socket.sendto(sip_string, (self.sender_ip, self.sender_port))
+
+            print(f"200 OK (Accept) sent to {self.sender_ip}. Waiting for ACK...")
+            return True
+
+        except Exception as e:
+            print(f"Error sending 200 OK for INVITE: {e}")
+            return False
         
-        start_line = SIP_packet.make_start_line_receive("200", "OK")
+    def recv_bye(self, packet):
+        try:
+            start_line = SIP_packet.make_start_line_receive("200", "OK")
 
-        sip = SIP_packet(start_line=start_line, 
-            via=packet.via,         
-            to=packet.to,           
-            _from=packet._from,     
-            call_id=packet.call_id,
-            cseq=packet.cseq,
-            content_type="application/sdp",
-            content_length=len(sdp),
-            body=sdp)
+            sip = SIP_packet(start_line=start_line, 
+                via=packet.via,         
+                to=packet.to,           
+                _from=packet._from,     
+                call_id=packet.call_id,
+                cseq=packet.cseq,
+                content_type=None,
+                content_length=0,
+                body="")
+            
+            sip_string = sip.to_bytes()
+
+            self.socket.sendto(sip_string, (self.sender_ip, self.sender_port))
+
+            print(f"200 OK (Accept) sent to {self.sender_ip} for BYE.")
+            return True
+
+        except Exception as e:
+            print(f"Error sending 200 OK for BYE: {e}")
+            return False
         
-        sip_string = sip.to_bytes()
+    def send_error(self, error_code, error_reason, packet):
+        try:
+            start_line = SIP_packet.make_start_line_receive(error_code, error_reason)
 
-        socket.sendto(sip_string, (sender_ip, sender_port))
+            sip = SIP_packet(
+                start_line=start_line, 
+                via=packet.via,         
+                to=packet.to,           
+                _from=packet._from,     
+                call_id=packet.call_id, 
+                cseq=packet.cseq,       
+                content_type=None,
+                content_length=0,
+                body=""
+            )
+            
+            sip_string = sip.to_bytes()
 
-        print(f"200 OK (Accept) sent to {sender_ip}. Waiting for ACK...")
-        return True
+            self.socket.sendto(sip_string, (self.sender_ip, self.sender_port))
 
-    except Exception as e:
-        print(f"Error sending 200 OK for INVITE: {e}")
-        return False
-    
-def recv_bye(packet, socket, sender_ip, sender_port):
-    try:
-        start_line = SIP_packet.make_start_line_receive("200", "OK")
-
-        sip = SIP_packet(start_line=start_line, 
-            via=packet.via,         
-            to=packet.to,           
-            _from=packet._from,     
-            call_id=packet.call_id,
-            cseq=packet.cseq,
-            content_type=None,
-            content_length=0,
-            body="")
-        
-        sip_string = sip.to_bytes()
-
-        socket.sendto(sip_string, (sender_ip, sender_port))
-
-        print(f"200 OK (Accept) sent to {sender_ip} for BYE.")
-        return True
-
-    except Exception as e:
-        print(f"Error sending 200 OK for BYE: {e}")
-        return False
-    
-def send_error(error_code, error_reason,packet, sender_ip, sender_port, socket):
-    try:
-        start_line = SIP_packet.make_start_line_receive(error_code, error_reason)
-
-        sip = SIP_packet(
-            start_line=start_line, 
-            via=packet.via,         
-            to=packet.to,           
-            _from=packet._from,     
-            call_id=packet.call_id, 
-            cseq=packet.cseq,       
-            content_type=None,
-            content_length=0,
-            body=""
-        )
-        
-        sip_string = sip.to_bytes()
-
-        socket.sendto(sip_string, (sender_ip, sender_port))
-
-        print(f"Error {start_line} sent to {sender_ip}.")
-        return True
-        
-    except Exception as e:
-        print(f"Failed to send error: {e}")
-        return False
+            print(f"Error {start_line} sent to {self.sender_ip}.")
+            return True
+            
+        except Exception as e:
+            print(f"Failed to send error: {e}")
+            return False
--- Connection_Functions/Send.py
+++ Connection_Functions/Send.py
@@ -1,85 +1,93 @@
 from Packets.SDP_packet import SDP_packet
 from Packets.SIP_packet import SIP_packet
 
-def send_invite(sender_ip, sender_port, receiver_ip, receiver_port, socket):
+class Send_func:
+    def __init__(self, sender_ip, sender_port, receiver_ip, receiver_port, socket):
+        self.sender_ip = sender_ip
+        self.sender_port = sender_port
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port
+        self.socket = socket
 
-    try:
-        #following protocol
-        sdp = SDP_packet(origin=f"- 676767 676767 IN IP {sender_ip}", connection_data=f"IN IP {sender_ip}", 
-                        #Depends on type of encoder (codec payload type 0 for media)  attribute clarifies payload type
-                        media=f"audio {sender_port} RTP/AVP 0", attribute="rtpmap:0 PCMU/8000").to_string()
+    def send_invite(self):
+
+        try:
+            #following protocol
+            sdp = SDP_packet(origin=f"- 676767 676767 IN IP {self.sender_ip}", connection_data=f"IN IP {self.sender_ip}", 
+                            #Depends on type of encoder (codec payload type 0 for media)  attribute clarifies payload type
+                            media=f"audio {self.sender_port} RTP/AVP 0", attribute="rtpmap:0 PCMU/8000").to_string()
+            
+            start_line = SIP_packet.make_start_line_send("INVITE", self.receiver_ip)
+
+            sip = SIP_packet(start_line=start_line, 
+                            via=f"SIP/2.0/UDP {self.sender_ip}:{self.sender_port}", 
+                            to=f"sip:{self.receiver_ip}", 
+                            _from=f"sip:{self.sender_ip}", 
+                            call_id="WEWANT4.0", 
+                            cseq="1 INVITE",
+                            content_type="application/sdp",
+                            content_length=len(sdp),
+                            body=sdp)
+            
+            sip_string = sip.to_bytes()
+
+            self.socket.sendto(sip_string, (self.receiver_ip, self.receiver_port))
+
+            print(f"INVITE successfully sent to {self.receiver_ip}:{self.receiver_port}")
+
+            return True
+        except Exception as e:
+            print(f"Error sending INVITE: {e}")
+            return False
         
-        start_line = SIP_packet.make_start_line_send("INVITE", receiver_ip)
+    def send_ack(self, packet):
 
-        sip = SIP_packet(start_line=start_line, 
-                         via=f"SIP/2.0/UDP {sender_ip}:{sender_port}", 
-                         to=f"sip:{receiver_ip}", 
-                         _from=f"sip:{sender_ip}", 
-                        call_id="WEWANT4.0", 
-                        cseq="1 INVITE",
-                        content_type="application/sdp",
-                        content_length=len(sdp),
-                        body=sdp)
-        
-        sip_string = sip.to_bytes()
+        try:
+            start_line = SIP_packet.make_start_line_send("ACK", self.receiver_ip)
 
-        socket.sendto(sip_string, (receiver_ip, receiver_port))
+            sip = SIP_packet(start_line=start_line, 
+                            via=f"SIP/2.0/UDP {self.sender_ip}:{self.sender_port}", 
+                            to=f"sip:{self.receiver_ip}", 
+                            _from=f"sip:{self.sender_ip}", 
+                            call_id=packet.call_id,
+                            cseq="1 ACK",
+                            content_type=None,
+                            content_length=0,
+                            body="")
+            
+            sip_string = sip.to_bytes()
 
-        print(f"INVITE successfully sent to {receiver_ip}:{receiver_port}")
+            self.socket.sendto(sip_string, (self.receiver_ip, self.receiver_port))
 
-        return True
-    except Exception as e:
-        print(f"Error sending INVITE: {e}")
-        return False
-    
-def send_ack(sender_ip, sender_port, receiver_ip, receiver_port, socket, packet):
+            print(f"ACK successfully sent to {self.receiver_ip}:{self.receiver_port}")
 
-    try:
-        start_line = SIP_packet.make_start_line_send("ACK", receiver_ip)
+            return True
+        except Exception as e:
+            print(f"Error sending ACK: {e}")
+            return False
 
-        sip = SIP_packet(start_line=start_line, 
-                         via=f"SIP/2.0/UDP {sender_ip}:{sender_port}", 
-                         to=f"sip:{receiver_ip}", 
-                        _from=f"sip:{sender_ip}", 
-                        call_id=packet.call_id,
-                        cseq="1 ACK",
-                        content_type=None,
-                        content_length=0,
-                        body="")
-        
-        sip_string = sip.to_bytes()
+    def send_bye(self, packet):
 
-        socket.sendto(sip_string, (receiver_ip, receiver_port))
+        try:
+            start_line = SIP_packet.make_start_line_send("BYE", self.receiver_ip)
 
-        print(f"ACK successfully sent to {receiver_ip}:{receiver_port}")
+            sip = SIP_packet(start_line=start_line, 
+                            via=f"SIP/2.0/UDP {self.sender_ip}:{self.sender_port}", 
+                            to=f"sip:{self.receiver_ip}", 
+                            _from=f"sip:{self.sender_ip}", 
+                            call_id=packet.call_id,
+                            cseq="2 BYE",
+                            content_type=None,
+                            content_length=0,
+                            body="")
+            
+            sip_string = sip.to_bytes()
 
-        return True
-    except Exception as e:
-        print(f"Error sending ACK: {e}")
-        return False
+            self.socket.sendto(sip_string, (self.receiver_ip, self.receiver_port))
 
-def send_bye(sender_ip, sender_port, receiver_ip, receiver_port, socket, packet):
+            print(f"BYE successfully sent to {self.receiver_ip}:{self.receiver_port}")
 
-    try:
-        start_line = SIP_packet.make_start_line_send("BYE", receiver_ip)
-
-        sip = SIP_packet(start_line=start_line, 
-                         via=f"SIP/2.0/UDP {sender_ip}:{sender_port}", 
-                         to=f"sip:{receiver_ip}", 
-                         _from=f"sip:{sender_ip}", 
-                        call_id=packet.call_id,
-                        cseq="2 BYE",
-                        content_type=None,
-                        content_length=0,
-                        body="")
-        
-        sip_string = sip.to_bytes()
-
-        socket.sendto(sip_string, (receiver_ip, receiver_port))
-
-        print(f"BYE successfully sent to {receiver_ip}:{receiver_port}")
-
-        return True
-    except Exception as e:
-        print(f"Error sending BYE: {e}")
-        return False
+            return True
+        except Exception as e:
+            print(f"Error sending BYE: {e}")
+            return False
