be7cf8cca53c6a07b233d2058ffd031a8c102bf5
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2026-04-05 23:46:42 +0800

    feat: added receive function for bonus

--- Bonus Specs/Twoway.py
+++ Bonus Specs/Twoway.py
@@ -2,8 +2,6 @@ import socket
 import threading
 import time
 import os
-import pyaudio
-import audioop
 
 #IP Configuration
 DEVICE1_IP = os.getenv("SENDER_IP")
@@ -11,64 +9,25 @@ DEVICE1_PORT = int(os.getenv("SENDER_PORT"))
 DEVICE2_IP = os.getenv("RECEIVER_IP")
 DEVICE2_PORT = int(os.getenv("RECEIVER_PORT"))
 
-#Microphone Configuration
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-VOICE_THRESHOLD = 800
-in_call = False
+is_connected = False    # True once the SIP ACK is exchanged
+is_calling = False      # True while waiting for a 200 OK
+flag = True     # Keeps the app alive
 
-def background_listener(shared_socket):
-    print("Listening for incoming packets...")
-    while True:
-        try:
-            data, addr = shared_socket.recvfrom(4096)
-        except socket.timeout:
-            continue # Just loop back and keep listening
-        except Exception as e:
-            # If the socket closes, the thread exits
-            break
-
-def mic_watcher(sip_sender):
-    global in_call
-    
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-    
-    print("Microphone is hot. Waiting for you to speak...")
-    
-    while True:
-        if not in_call:
-            # 1. Grab a tiny slice of audio (about 0.02 seconds)
-            data = stream.read(CHUNK)
-            
-            # 2. Calculate how loud that slice is
-            rms_volume = audioop.rms(data, 2) 
-            
-            # 3. If the volume spikes above the threshold, trigger the call!
-            if rms_volume > VOICE_THRESHOLD:
-                print(f"\n[VOX DETECTED] Volume spiked to {rms_volume}!")
-                print(f">>> Auto-sending INVITE to {target_ip}... >>>")
-                
-                # Fire your SIP function
-                sip_sender.send_invite(target_ip)
-                
-                # Lock the state so we don't spam 500 INVITEs per second
-                in_call = True 
-                
-        else:
-            # If we are already in a call, we don't need to trigger an INVITE.
-            # We just sleep for a bit to let Jeru's RTP threads handle the actual speaking.
-            time.sleep(0.1)
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
 sock.bind((DEVICE1_IP, DEVICE1_PORT))
-sock.settimeout(2.0)
 
-listener_thread = threading.Thread(target=background_listener, args=(sock,), daemon=True)
-listener_thread.start()
+print(f"UDP server listening on {DEVICE1_IP}:{DEVICE1_PORT}\n")
 
-vox_thread = threading.Thread(target=mic_watcher, args=(sender_object,), daemon=True)
-vox_thread.start()
+while flag:
+    if not is_connected:
+        command = input("\nType 'I' to invite or 'Q' to quit: ").upper()
+        if command == 'I':
+                target = input("Enter Target IP: ")
+                # Trigger SIP_Sender.send_invite(target)
+        elif command == 'Q':
+                is_listening = False
+    else:
+        command = input("\nType 'B' to disconnect: ").upper()
+        if command == 'B':
+            # Trigger SIP_Sender.send_bye()
+            is_connected = False
